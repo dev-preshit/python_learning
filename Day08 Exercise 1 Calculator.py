049c0225d06a26b2601d0e3e5e0753f26ec5b1f1
dev-preshit <preshit.kamble17705@gmail.com>
2024-09-22 20:42:02 +0530

    nameupdate

---
+++ Day08 Exercise 1 Calculator.py
@@ -0,0 +1,25 @@
+#EXECRISE 1
+#SIMPLE CALCULATOR
+# WAP in python to create a calculator
+
+
+a=float(input("Enter first value"))
+b=float(input("Enter second value"))
+n=str(input("Enter an operator value"))
+
+if n == '+' :
+    print("ANS: ",a+b)
+elif n == '-':
+    print("ANS: ",a-b)
+elif n == '*':
+    print("ANS: ",a*b)
+elif n == '/':
+    print("ANS: ",a/b)
+elif n == '%':
+    print("ANS: ",a%b)
+elif n == '**':
+    print("ANS: ",a**b)
+elif n == '//':
+    print("ANS: ",a//b)
+else:
+    print("WRONG CODE")
