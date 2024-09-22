0b0863f8e3804878db04046f6a7e7c9bcabaaff7
dev-preshit <preshit.kamble17705@gmail.com>
2024-09-22 20:41:37 +0530

    nameupdate

---
+++ Day07 Operator.py
@@ -0,0 +1,39 @@
+"""
+    # day 7 is about operators
+        in any programming launguage the special symbol use for certain purpose are known  as operators...
+
+            1)Arithamatic operator
+                • Addition          (+)
+                • Substraction      (-)
+                • Multiplication    (*)
+                • Division          (/)
+                • Modulus           (%)
+                • Exponatial        (**)
+                • Floor Division    (//)
+
+            this above are different types of arithamatic operator use to do arithamatic operation
+"""
+
+
+a=20
+b=3
+
+print("Addition         (+)",a+b)
+print("Substraction     (-)",a-b)
+print("Multiplication   (*)",a*b)
+print("Division         (/)",a/b)
+print("Modulus          (%)",a%b)
+print("Exponatial       (**)",a**b)
+print("Floor Division   (//)",a//b)
+
+"""
+OUTPUT:
+        Addition         (+) 23
+        Substraction     (-) 17
+        Multiplication   (*) 60
+        Division         (/) 6.666666666666667
+        Modulus          (%) 2
+        Exponatial       (**) 8000
+        Floor Division   (//) 6
+
+"""
