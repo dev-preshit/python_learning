bf0f3eea1b879b881082387cd9e7f8ff9936aba8
dev-preshit <preshit.kamble17705@gmail.com>
2024-09-26 22:30:57 +0530

    Day 29

---
+++ Day29 Docstring.py
@@ -0,0 +1,22 @@
+"""
+    #Day29 is about DocSting in python...
+        • Docstrings in python
+            Python docstrings are the string literals that appear
+            right after the definition of a function, method, class,
+            or module.
+            eg.
+                def value(n):
+                    '''This is Docstring which is not comment and 
+                    python interpreter store this string in doc attribute'''
+                    print(n)
+                value(5)
+                print(value.__doc__)
+"""
+
+def value(n):
+    '''This is Docstring which is not comment and 
+python interpreter store this string in doc attribute'''
+    print(n)
+
+value(5)
+print(value.__doc__)
