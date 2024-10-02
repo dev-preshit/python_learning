03dd1cfb69dfe716c845c0f60a81696923ed8ef5
dev-preshit <preshit.kamble17705@gmail.com>
2024-10-02 23:53:00 +0530

    Day-40

--- Day40 Exercise 4 Secret Code Language.py
+++ Day40 Exercise 4 Secret Code Language.py
@@ -9,20 +9,54 @@ def coder(string):
     if n == 1 or n == 2:
             return string[::-1]
     else:
-            i = 0
-            string += string[0]
-            string = string.replace(string[0],"",1)
-            while i < 3:
-                no = chr(random.randint(97,122))
-                string = string + no
-                i = i+1 
-            while i < 6:
-                no = chr(random.randint(97,122))
-                string =  no + string
-                i = i+1 
+            fchar = string[0]
+            string = string[1:] + fchar
+            for _ in range(3):
+                string += chr(random.randint(97,122))
+            for _ in range(3):
+                string = chr(random.randint(97,122)) + string
             return string
 
-Strinput = input("Enter Message: ")
-stringlist = Strinput.split(" ")
-for i in stringlist:
-    print(coder(i), end = " ")
+
+def Decoder(Dstring):
+    n = len(Dstring)
+    if n == 1 or n == 2:
+        return Dstring[::-1]
+    else:
+        Dstring = Dstring[3:-3]
+        lchar = Dstring[-1]
+        Dstring = lchar + Dstring[:-1]
+        return Dstring
+
+def Cinput():
+
+    Strinput = input("Enter Message: ")
+    stringlist = Strinput.split(" ")
+    replylist = []
+
+    for i in stringlist:
+        code = coder(i)
+        replylist.append(code)
+    
+    Strreply = " ".join(replylist)
+    print("coded message:", Strreply)
+    return Strreply
+
+def Dinput():
+    Dstrinput = input("Enter message to encode: ")
+    stringlist = Dstrinput.split(" ")
+    replylist = []
+    for i in stringlist:
+        dcode = Decoder(i)
+        replylist.append(dcode)
+
+    Strreply = " ".join(replylist)
+    print("Decoded message:", Strreply)
+    return Strreply
+
+     
+print("Welcome to Coder and De-coder of message")
+# print("Do you want to code or de-code")
+
+Cinput()
+Dinput()
