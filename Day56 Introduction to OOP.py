cbe3f8b0f91edef4ec015dfb40a2b684d36fde32
dev-preshit <preshit.kamble17705@gmail.com>
2024-10-10 23:27:51 +0530

    Day-56

---
+++ Day56 Introduction to OOP.py
@@ -0,0 +1,34 @@
+"""
+    #Day 56 is about Introduction to OOP in Python...
+        In programming language there are two appoachers that are used to  write program
+        1) Procedural Programming
+        2) Object-Oriented Programming
+
+        The procedure we are following till now is the "Procedural Programming" approach.
+        So, in this session, we will learn about Object Oriented Programming (OOP). The
+        basic idea of object-oriented programming (OOP) in Python is to use classes and
+        objects to represent real-world concepts and entities.
+        eg. 
+            You are having a compitation in your college for that you want all participants
+            To fill a form which have their name, branch and semister.
+            for that you create a google form which will have certain coloum for name, branch
+            and semister this will be considered as template or blueprint.
+            While the individual student filling the form will become a object which is a 
+            Real-World Entity...
+
+        CLASS:
+            A class is a blueprint or template for creating objects. It defines the properties and
+            methods that an object of that class will have. Properties are the data or state of an
+            object, and methods are the actions or behaviors that an object can perform.
+
+        OBJECT:
+            An object is an instance of a class, and it contains its own data and methods. For example,
+            you could create a class called "Person" that has properties such as name and age, and
+            methods such as speak() and walk(). Each instance of the Person class would be a unique
+            object with its own name and age, but they would all have the same methods to speak and walk.
+
+        4 pillars of OOP...
+            1)Encapsulation.
+            2)Inheritance.
+            3)Polymorphism.
+"""
