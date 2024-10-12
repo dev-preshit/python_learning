4c2e23d3f8a27701fde00f8ae0e79dffa086d44e
dev-preshit <preshit.kamble17705@gmail.com>
2024-10-13 01:01:23 +0530

    Day-60

--- Day60 Getters and Setters.py
+++ Day60 Getters and Setters.py
@@ -12,7 +12,8 @@
                 @property   #This is getter
                 def ten_value(self):
                     return 10* self.value
-
+                    
+        Setters:
             It is important to note that the getters do not take any parameters and we cannot
             set the value through getter method.For that we need setter method which can be
             added by decorating method with @property_name.setter.
