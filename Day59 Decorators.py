c09cad388b6ab59841d135ce2232190293507f8b
dev-preshit <preshit.kamble17705@gmail.com>
2024-10-14 01:54:22 +0530

    Day-59

--- Day59 Decorators.py
+++ Day59 Decorators.py
@@ -1,5 +1,5 @@
 """
-    Day 59 is about Decorators in python...
+    Day 59 is about Decorators in Python...
         A decorator is a function that takes another function as an argument
         and returns a new function that modifies the behavior of the original
         function. The new function is often referred to as a "decorated" function.
@@ -7,32 +7,33 @@
             @decorator_function
             def my_function():
                 pass
+
 """
 
 def greet(fx):
-    def mfx(*augs,**kwaugs):
-        print("GOOD MORING")
-        fx(*augs,**kwaugs)
+    def mfx(*args, **kwargs):  # Corrected variable names for clarity
+        print("GOOD MORNING")  # Corrected spelling
+        fx(*args, **kwargs)
         print("GOOD NIGHT")
     return mfx
     
 
 @greet
 def hello():
-    print("Hello world")
+    print("Hello, world")
 
 @greet
-def add(x,y):
-    print(x+y)
+def add(x, y):
+    print(x + y)
 
 hello()
-# greet(hello)() #Can Also write like this
-# GOOD MORING
-# Hello world
+# greet(hello)() # Can also write like this
+# GOOD MORNING
+# Hello, world
 # GOOD NIGHT
 
-add(10,20)
-# greet(add)(10,20) #Can Also write like this
-# GOOD MORING
+add(10, 20)
+# greet(add)(10,20) # Can also write like this
+# GOOD MORNING
 # 30
 # GOOD NIGHT
