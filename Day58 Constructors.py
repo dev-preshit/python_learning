d13d06200cb1bd672dba0ada945c0e83aa1a37b9
dev-preshit <preshit.kamble17705@gmail.com>
2024-10-14 01:57:30 +0530

    Day-58

--- Day58 Constructors.py
+++ Day58 Constructors.py
@@ -1,22 +1,23 @@
 """
-    #Day 58 is about Constructors in Python...
-        A constructor is a function in class which is called automatically
-        when a object is created.
-        This function is generally use to initilise the value we want
-        at the time of object creation.
-        To create a constructor we have to use __init__() keyword
-        eg.
-            def __init__()
+    # Day 58 is about Constructors in Python...
+    A constructor is a function in a class that is called automatically
+    when an object is created.
+    This function is generally used to initialize the values we want
+    at the time of object creation.
+    To create a constructor, we have to use the __init__() method.
+    e.g.,
+        def __init__():
+
 """
 
 class Person:
-    def __init__(self,nm,age):
+    def __init__(self, nm, age):
         self.name = nm
         self.age = age
 
     def info(self):
-       print(f'{self.name} is {self.age} year old')
+       print(f'{self.name} is {self.age} years old')  # Changed "year" to "years" for grammatical correctness
 
-a = Person('ram',15) 
+a = Person('Ram', 15) 
 a.info()
-# ram is 15 year old
+# Ram is 15 years old
