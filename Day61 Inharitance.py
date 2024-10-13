d13bd363feaf93f71217449d59367c8f3d60107a
dev-preshit <preshit.kamble17705@gmail.com>
2024-10-14 01:50:35 +0530

    Day-61

--- Day61 Inharitance.py
+++ Day61 Inharitance.py
@@ -1,34 +1,36 @@
 """
-    #Day 61 is about Inharitance in Python...
-        Using one class variable and method into another class is
-        a general idea of Inharitance 
-        The class from which the variable and methon is been inharite 
-        is called Parent class or Base class and The class which is using this properties
-        is called Child class or Dericed class resp.
+        # Day 61 is about Inheritance in Python...
+        Using one class's variables and methods in another class is
+        the general idea of Inheritance.
+        The class from which the variables and methods are inherited 
+        is called the Parent class or Base class, and the class which is using these properties
+        is called the Child class or Derived class, respectively.
         
         class BaseClass:
             pass
         class DerivedClass(BaseClass):
             pass 
 
-        Derived class inherits features from the base class where new features can
-        be added to it. This results in re-usability of code.
+        The Derived class inherits features from the Base class, where new features can
+        be added to it. This results in code reusability.
+
 """
 
 class Employee:
-    def __init__(self,name,id):
+    def __init__(self, name, id):
         self.name = name
         self.id = id
     
     def show(self):
         print(f"The name of Employee {self.id} is {self.name}")
+
 class Programmer(Employee):
-    def add(self,x,y):
-        return x+y
+    def add(self, x, y):
+        return x + y
     
-e = Employee("Narendra",420)
+e = Employee("Narendra", 420)
 e.show()
-p2 = Programmer("Ramcharan",123)
-print(p2.add(10,20))
-p2.show()
-# show is method of Employee Class but we are able to use it using object of Programmer Class
+p2 = Programmer("Ramcharan", 123)
+print(p2.add(10, 20))
+p2.show()  
+# 'show' is a method of the Employee class, but we are able to use it with an object of the Programmer class.
