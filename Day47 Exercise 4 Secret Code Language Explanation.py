f582104c136964edbd6fd3306fd4d241a75bf835
dev-preshit <preshit.kamble17705@gmail.com>
2024-10-15 23:03:18 +0530

    Day-47

--- Day47 Exercise 4 Secret Code Language Explanation.py
+++ Day47 Exercise 4 Secret Code Language Explanation.py
@@ -1,47 +1,46 @@
-#EXECRISE 4
-# Secret Code language explanation
-#create a coder and de-coder for string secret language...
+# EXERCISE 4
+# Secret Code Language Explanation
 """
-    So I have created two function named as coder and decoder doing 
-    work to code and decode string.
+    I have created two functions named coder and decoder, which work 
+    to code and decode strings.
 
-    It first check if the string length is 1 or 2 if yes than it  
-    simply reverse the string and return it, or else it take first 
-    character for words and adding it to last than generating 6 random 
-    number converting it to character and adding it at front and back.
+    The coder function first checks if the string length is 1 or 2. If yes, 
+    it simply reverses the string and returns it. Otherwise, it takes the 
+    first character of the word, adds it to the end, generates 6 random 
+    numbers, converts them to characters, and adds them to the front and back 
+    of the string.
 
-    This coder function is been called by Cinput function where the string 
-    get split by split function and get stored in list. than the list in 
-    intrated and call coder funtion for each word after each word is coded it return
-    it back to Cinput where this again get store in list and get converted back in string
+    This coder function is called by the Cinput function, where the string 
+    gets split by the split function and is stored in a list. Then the list 
+    is iterated, calling the coder function for each word. After each word 
+    is coded, it is returned to Cinput, where it is stored in a list and 
+    converted back into a string.
 
-    Same with decoder function as it remove random words from start and end
-    and than add last character at first place if word have more than 3 character
-    or else ir simply reverse it.
+    The decoder function does the reverse: it removes random characters 
+    from the start and end, then adds the last character to the first place 
+    if the word has more than 3 characters; otherwise, it simply reverses it.
 
-    This decoder funtion get its input from Dinput where the input string get slice
-    and been sent to decoder function which return encoded word which leter been add to 
-    a list and been created a string using jion method...
+    This decoder function gets its input from Dinput, where the input string 
+    is sliced and sent to the decoder function, which returns the decoded word, 
+    later added to a list, creating a string using the join method...
 """
-
 
 import random
 
 def coder(string):
     n = len(string)
     if n == 1 or n == 2:
-            return string[::-1]
+        return string[::-1]
     else:
-            fchar = string[0]
-            string = string[1:] + fchar
-            for _ in range(3):
-                string += chr(random.randint(97,122))
-            for _ in range(3):
-                string = chr(random.randint(97,122)) + string
-            return string
+        fchar = string[0]
+        string = string[1:] + fchar
+        for _ in range(3):
+            string += chr(random.randint(97, 122))
+        for _ in range(3):
+            string = chr(random.randint(97, 122)) + string
+        return string
 
-
-def Decoder(Dstring):
+def decoder(Dstring):
     n = len(Dstring)
     if n == 1 or n == 2:
         return Dstring[::-1]
@@ -52,7 +51,6 @@ def Decoder(Dstring):
         return Dstring
 
 def Cinput():
-
     Strinput = input("Enter Message: ")
     stringlist = Strinput.split(" ")
     replylist = []
@@ -62,24 +60,23 @@ def Cinput():
         replylist.append(code)
     
     Strreply = " ".join(replylist)
-    print("coded message:", Strreply)
+    print("Coded message:", Strreply)
     return Strreply
 
 def Dinput():
-    Dstrinput = input("Enter message to encode: ")
+    Dstrinput = input("Enter message to decode: ")
     stringlist = Dstrinput.split(" ")
     replylist = []
+
     for i in stringlist:
-        dcode = Decoder(i)
+        dcode = decoder(i)
         replylist.append(dcode)
 
     Strreply = " ".join(replylist)
     print("Decoded message:", Strreply)
     return Strreply
 
-     
-print("Welcome to Coder and De-coder of message")
-# print("Do you want to code or de-code")
+print("Welcome to the Coder and Decoder of messages")
 
 Cinput()
 Dinput()
