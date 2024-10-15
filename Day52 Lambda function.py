30c77b9f125aa92ce788726643db4973b658ea9f
dev-preshit <preshit.kamble17705@gmail.com>
2024-10-15 22:55:13 +0530

    Day-52

--- Day52 Lambda function.py
+++ Day52 Lambda function.py
@@ -1,36 +1,36 @@
 """
-    #Day52 is about lambda function in python...
+    # Day 52 is about lambda functions in Python...
         In Python, a lambda function is a small anonymous function without a name.
-        It is defined using the lambda keyword 
-        syntax:
+        It is defined using the lambda keyword.
+        Syntax:
             lambda arguments: expression
 
-        We can also pass function into function as arguments
-        
+        We can also pass functions into other functions as arguments.
 """
-# function using def 
+
+# Function using def 
 def double(x):
-    return x*2
+    return x * 2
 
 print(double(5))
 # 10
 
-#function using lambda
-twice = lambda x: x*2
+# Function using lambda
+twice = lambda x: x * 2
 print(twice(10))
 # 20
 
-avg = lambda x,y,z: (x+y+z)/3
+avg = lambda x, y, z: (x + y + z) / 3
 print(avg(10, 20, 30))
 # 20
 
-#Function passed in function
-def apply(fx , value):
-    return fx(value) * fx(value-1)
+# Function passed into a function
+def apply(fx, value):
+    return fx(value) * fx(value - 1)
 
 print(apply(double, 5))
 # 80
 
-#we can also pass lambda function instead proper function
-print(apply(lambda x:x*2, 5))
-# 80 
+# We can also pass a lambda function instead of a proper function
+print(apply(lambda x: x * 2, 5))
+# 80
