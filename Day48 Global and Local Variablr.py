3b709ee903f026ce55ea53ac269221d08409dd7d
dev-preshit <preshit.kamble17705@gmail.com>
2024-10-15 23:02:09 +0530

    Day-48

--- Day48 Global and Local Variablr.py
+++ Day48 Global and Local Variablr.py
@@ -1,43 +1,34 @@
 """
-    #Day 48 is about Global and Local variable in Python...
-        Global variables are the varibles which are acessable through out the whole code
-        while Local variable can only be access in a function 
-        Variable which are declared inside a function is local vaiable which are only accessable
-        in that function only , but is we declare a variable outside of function than we can use 
-        that variable inside function as long it is not declared in function to making 
-        it a global variable...
+    # Day 48 is about Global and Local variables in Python...
+        Global variables are the variables that are accessible throughout the whole code,
+        while local variables can only be accessed within a function.
+        Variables that are declared inside a function are local variables, which are only accessible
+        within that function. However, if we declare a variable outside of a function, we can use 
+        that variable inside the function as long as it is not redeclared in the function, 
+        making it a global variable...
         
-        What if we want to change value on global variable inside function
-        we can do so by using global keyword
-        Global Keyword is use as prefix to variable which tell that the variable is 
-        not declared in function but the global variable is been used...
+        What if we want to change the value of a global variable inside a function?
+        We can do so by using the global keyword.
+        The global keyword is used as a prefix to a variable to indicate that the variable is 
+        not declared in the function but is a global variable being used...
 """
-x = 10
-print(x)
 
-# 10
-print(f" global x is {x}")
-#  global x is 10
+x = 10
+print(x)  # 10
+print(f"Global x is {x}")  # Global x is 10
 
 y = 5
-print(f" global y is {y}")
-#  global y is 5
+print(f"Global y is {y}")  # Global y is 5
 
 def hello():
     x = 20
     global y
     y = y + 2
-    print(f"    local x is {x}")
-#     local x is 20
-    print(f"    global y is {y}")
-#     global y is 7
-    print("    i am inside function")
-#     i am inside function
+    print(f"    Local x is {x}")  # Local x is 20
+    print(f"    Global y is {y}")  # Global y is 7
+    print("    I am inside the function")  # I am inside the function
 
 hello()
 
-print(f" global x is {x}")
-#  global x is 10
-
-print(f" global y is {y}")
-#  global y is 7
+print(f"Global x is {x}")  # Global x is 10
+print(f"Global y is {y}")  # Global y is 7
