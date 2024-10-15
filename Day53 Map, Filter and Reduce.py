6d4c129f945cdbc89b9274fa0ca8a135c0ef1eab
dev-preshit <preshit.kamble17705@gmail.com>
2024-10-15 22:53:24 +0530

    Day-53

--- Day53 Map, Filter and Reduce.py
+++ Day53 Map, Filter and Reduce.py
@@ -1,5 +1,5 @@
 """
-    #Day 53 is about Map, Filter and Reduce in python...
+    # Day 53 is about Map, Filter, and Reduce in Python...
         In Python, the map, filter, and reduce functions are built-in functions that allow
         you to apply a function to a sequence of elements and return a new sequence.
 
@@ -8,74 +8,72 @@
             a new sequence containing the transformed elements.
             Syntax:
                 map(function, iterable)
-            Lambda function will also work...
-            Will return a map object and will be need to typecast it into list, tuple, or set.
+            Lambda functions will also work...
+            It will return a map object, which will need to be typecast into a list, tuple, or set.
 
         filter():
             The filter function filters a sequence of elements based on a given predicate (a
             function that returns a boolean value) and returns a new sequence containing
             only the elements that meet the predicate. 
             Syntax:
-                filter(predicate,iterable)
-            Lambda function will also work...
-            Will return a filter object and will be need to typecast it into list, tuple, or set.
+                filter(predicate, iterable)
+            Lambda functions will also work...
+            It will return a filter object, which will need to be typecast into a list, tuple, or set.
         
         reduce():
             The reduce function is a higher-order function that applies a function to a
-            sequence and returns a single value. It is a part of the functools module in Python
+            sequence and returns a single value. It is part of the functools module in Python.
             Syntax:
-                reduce(function,iterable)
-            Lambda function will also work...
-            Will return a reduce value and will not be need to typecast it into list, tuple, or set
-            as it is a single value
+                reduce(function, iterable)
+            Lambda functions will also work...
+            It will return a reduced value and will not need to be typecast into a list, tuple, or set
+            as it is a single value.
 """
-#MAP() FUNCTION
-def cube(x):
-    return x*x*x
 
-l = [1,2,3,4,5]
+# MAP() FUNCTION
+def cube(x):
+    return x * x * x
+
+l = [1, 2, 3, 4, 5]
 
 # newl = []
 # for i in l:
 #     newl.append(cube(i))
 # [1, 8, 27, 64, 125]
 
-Mapl = list(map(cube,l))
+Mapl = list(map(cube, l))
 # [1, 8, 27, 64, 125]
 print(Mapl)
-# both will give the same answer but the length of code is reduced
+# Both will give the same answer, but the length of code is reduced.
 
 
 
-
-#FILTER() FUNCTION
+# FILTER() FUNCTION
 def filter_fx(x):
     return x > 0
 
-l = [1,-7,0,-4,5]
+l = [1, -7, 0, -4, 5]
 
 # Fill = []
 # for i in l:
-#     if True == filter_fx(i) :
+#     if True == filter_fx(i):
 #         Fill.append(i)
-# [1 ,5]
+# [1, 5]
 
-
-Fill = list(filter(filter_fx,l))
-# [1 ,5]
+Fill = list(filter(filter_fx, l))
+# [1, 5]
 print(Fill)
-# both will give the same answer but the length of code is reduced
+# Both will give the same answer, but the length of code is reduced.
 
 
 
-
-#REDUCE() FUNCTION
+# REDUCE() FUNCTION
 from functools import reduce
  
 def pro(x, y):
     return x * y
 
-l = [1,2,3,4,5]
-Redl = reduce(pro,l)
+l = [1, 2, 3, 4, 5]
+Redl = reduce(pro, l)
 print(Redl)
 # 120
