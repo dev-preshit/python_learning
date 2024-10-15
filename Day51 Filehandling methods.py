419f518c388eb459a8896de95bf29b9999f4c690
dev-preshit <preshit.kamble17705@gmail.com>
2024-10-15 22:57:24 +0530

    Day-51

--- Day51 Filehandling methods.py
+++ Day51 Filehandling methods.py
@@ -1,41 +1,40 @@
 """
-    #Day 51 is about seek() and tell() function in python...
+    # Day 51 is about the seek() and tell() functions in Python...
         In Python, the seek() and tell() functions are used to work with file
         objects and their positions within a file. These functions are part of
-        the built-in IO module, which provides a consistent interface for
+        the built-in I/O module, which provides a consistent interface for
         reading and writing to various file-like objects, such as files, pipes,
         and in-memory buffers.
 
         seek() function:
-            Point the character position passed as argument, in byte in seek function...
+            Points to the character position passed as an argument, in bytes in the seek function...
         
         tell() function:
-            Return the current position within the file, in byte...
+            Returns the current position within the file, in bytes...
 
         truncate() function:
-            truncate the file to a specific size...
+            Truncates the file to a specific size...
 """
 
-with open("Day51 myfile.txt","r") as f:
-    #move to the 15th byte in the file
+with open("Day51 myfile.txt", "r") as f:
+    # Move to the 5th byte in the file
     f.seek(5)
 
-    #return current position of pointer
+    # Return current position of the pointer
     pos = f.tell()
     print(pos)
     # 5
 
-    #read the next 5 byte after 5th byte
+    # Read the next 5 bytes after the 5th byte
     text = f.read(5)    
     print(text)
     # 6789A
 
-with open("Day51 myfile.txt","a") as f:
+with open("Day51 myfile.txt", "a") as f:
     f.write("\nHello world!!")
     f.truncate(23)
 
-with open("Day51 myfile.txt","r") as f:
+with open("Day51 myfile.txt", "r") as f:
     print(f.read())
     # 123456789ABCDEF
     # Hello
-
