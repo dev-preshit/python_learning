19ba3f3ba257dd227c46e70bf1d36e17cf383160
dev-preshit <preshit.kamble17705@gmail.com>
2024-10-17 00:59:39 +0530

    Day-41

--- Day41 Short hand if else .py
+++ Day41 Short hand if else .py
@@ -1,15 +1,15 @@
 """
-    #Day 41 is about Short hand If Else Statement in python...
-        There is also a shorthand syntax for the if-
-        else statement that can be used when the
-        condition being tested is simple and the
-        code blocks to be executed are short.
+    #Day 41 is about Short-hand If-Else Statement in Python...
+        There is also a shorthand syntax for the if-else statement that can be used
+        when the condition being tested is simple, and the code blocks to be executed are short.
+        
         Syntax: 
-            exp-1 if cond-1 else exp-2 if cond-2 else exp-3
-        eg.
+            exp1 if cond1 else exp2 if cond2 else exp3
+        
+        Example:
             a = 10
-            b = 20 
-            print("A") if a<b else print("equal") if a == b else print("B")  
+            b = 20
+            print("A") if a < b else print("equal") if a == b else print("B")  
 """
 
 check = 0
@@ -17,10 +17,10 @@ print("Zero") if check == 0 else print("Non-Zero")
 # Zero
 
 a = 10
-b = 20 
-c = 1 if a>b else 0
+b = 20
+c = 1 if a > b else 0
 print(c)
 # 0
 
-print("A") if a>b else print("equal") if a == b else print("B") 
+print("A") if a > b else print("equal") if a == b else print("B")
 # B
