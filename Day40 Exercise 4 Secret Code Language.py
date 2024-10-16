7f4f9b91fff6cc892a6cb5297f973d2f1e737586
dev-preshit <preshit.kamble17705@gmail.com>
2024-10-17 01:13:45 +0530

    Day-40

--- Day40 Exercise 4 Secret Code Language.py
+++ Day40 Exercise 4 Secret Code Language.py
@@ -1,24 +1,23 @@
-#EXECRISE 4
-# Secret Code language
-#create a coder and de-coder for string secret language...
+# EXERCISE 4
+# Secret Code Language
+# Create a coder and decoder for a string secret language...
 
 import random
 
 def coder(string):
     n = len(string)
     if n == 1 or n == 2:
-            return string[::-1]
+        return string[::-1]
     else:
-            fchar = string[0]
-            string = string[1:] + fchar
-            for _ in range(3):
-                string += chr(random.randint(97,122))
-            for _ in range(3):
-                string = chr(random.randint(97,122)) + string
-            return string
+        fchar = string[0]
+        string = string[1:] + fchar
+        for _ in range(3):
+            string += chr(random.randint(97, 122))
+        for _ in range(3):
+            string = chr(random.randint(97, 122)) + string
+        return string
 
-
-def Decoder(Dstring):
+def decoder(Dstring):
     n = len(Dstring)
     if n == 1 or n == 2:
         return Dstring[::-1]
@@ -29,7 +28,6 @@ def Decoder(Dstring):
         return Dstring
 
 def Cinput():
-
     Strinput = input("Enter Message: ")
     stringlist = Strinput.split(" ")
     replylist = []
@@ -39,24 +37,22 @@ def Cinput():
         replylist.append(code)
     
     Strreply = " ".join(replylist)
-    print("coded message:", Strreply)
+    print("Coded message:", Strreply)
     return Strreply
 
 def Dinput():
-    Dstrinput = input("Enter message to encode: ")
+    Dstrinput = input("Enter message to decode: ")
     stringlist = Dstrinput.split(" ")
     replylist = []
+
     for i in stringlist:
-        dcode = Decoder(i)
+        dcode = decoder(i)
         replylist.append(dcode)
 
     Strreply = " ".join(replylist)
     print("Decoded message:", Strreply)
     return Strreply
 
-     
-print("Welcome to Coder and De-coder of message")
-# print("Do you want to code or de-code")
-
+print("Welcome to the Coder and Decoder of messages")
 Cinput()
 Dinput()
