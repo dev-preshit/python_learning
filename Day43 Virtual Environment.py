34e08a19b059191b5e37b816e47c957ad89542d6
dev-preshit <preshit.kamble17705@gmail.com>
2024-10-17 00:57:42 +0530

    Day-43

--- Day43 Virtual Environment.py
+++ Day43 Virtual Environment.py
@@ -1,33 +1,34 @@
 """
-    #Day 43 is about Virtual Environment in Python...
+    # Day 43 is about Virtual Environment in Python...
         A virtual environment is a tool used to isolate specific 
         Python environments on a single machine, allowing you to work
         on multiple projects with different dependencies and packages 
         without conflicts. This can be especially useful when
         working on projects that have conflicting package versions 
         or packages that are not compatible with each other.
+
         To create a virtual environment in Python, you can use the venv 
         module that comes with Python. Here's an example of
         how to create a virtual environment and activate it.
 """
 """
-    # Create a virtual environment
-        python -m venv myenv(env_name)
+    # Create a virtual environment:
+        python -m venv myenv (env_name)
 
-    # Activate the virtual environment (Linux/mac0S)
+    # Activate the virtual environment (Linux/macOS):
         source myenv/bin/activate
 
-    # Activate the virtual environment (Windows)
-        myenv/Scripts/activate.bat (ps1 if working in powershell)
+    # Activate the virtual environment (Windows):
+        myenv/Scripts/activate.bat (use activate.ps1 if working in PowerShell)
 
-    # Deactivate the virtual environment
+    # Deactivate the virtual environment:
         deactivate
 
-    # Output the list of installed packages and their versions to a file
-        pip freeze > requirements. txt
-    This will create a requirements .txt file that will have data 
-    of packages with there version
+    # Output the list of installed packages and their versions to a file:
+        pip freeze > requirements.txt
+    This will create a requirements.txt file that will contain data 
+    of packages with their versions.
 
-    # Install the packages listed in the requirements. txt file
-        pip install -r requirements .txt
+    # Install the packages listed in the requirements.txt file:
+        pip install -r requirements.txt
 """
